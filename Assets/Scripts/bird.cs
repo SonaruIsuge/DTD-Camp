1ad8a47f755a4b12763716fcab3059cd55fc0c7f
SonaruIsuge <bread511206@gmail.com>
2021-04-21 14:42:41 +0800

    加上註解

--- Assets/Scripts/bird.cs
+++ Assets/Scripts/bird.cs
@@ -6,56 +6,54 @@ using UnityEngine.SceneManagement;
 
 public class bird : MonoBehaviour
 {
-    public float FlySpeed;
-    public int score = 0;
+    /* 變數宣告在這裡 */
+    public float FlySpeed;  //(小數)變數 : 鳥向上飛的速度
+    public int score;   //(整數)變數 : 分數
+    public Rigidbody2D rigidBody2d; //(2D物理控制器)變數 : 物理控制器
+    public Text scoreTxt;   //(文字元件)變數 : 分數文字
+    public Text GameOverTxt;    //(文字元件)變數 : 遊戲結束文字
+    bool gameOver = false;  //(布林)變數 : 是否遊戲結束
 
-    public Rigidbody2D rb;
-    public Text scoreTxt;
-    public Text GameOverTxt;
 
-    bool gameOver = false;
-
-    // Start is called before the first frame update
+    // 遊戲開始時執行一次
     void Start()
     {
-        GameOverTxt.enabled = false;
-        rb = GetComponent<Rigidbody2D>();
+        GameOverTxt.enabled = false;    //遊戲開始時把遊戲結束文字隱藏
     }
 
-    // Update is called once per frame
+    // 遊戲進行中每偵執行
     void Update()
     {
-        if (gameOver)
-        {
-            GameOverTxt.enabled = true;
-            score = 0;
-            Time.timeScale = 0;
-            if (Input.GetMouseButtonDown(0))
-            {
-                SceneManager.LoadScene("mainScene");
-                Time.timeScale = 1;
-            }
-        }
-        else
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                rb.velocity = Vector3.up * FlySpeed;
-            }
-        }
-        
+        if (gameOver)                                           //  當遊戲結束時 : 
+        {                                                       //  {
+            GameOverTxt.enabled = true;                         //      讓GameOver文字顯現
+            Time.timeScale = 0;                                 //      遊戲時間暫停
+
+            if (Input.GetMouseButtonDown(0))                    //      當按下滑鼠左鍵時 : 
+            {                                                   //      {
+                SceneManager.LoadScene("mainScene");            //          載入名為"mainScene"的場景 (會讓程式從新開始運行，變數歸零)
+                Time.timeScale = 1;                             //          遊戲時間恢復正常
+            }                                                   //      }
+        }                                                       //  }
+        else                                                    //  當遊戲還沒結束(也就是進行中)
+        {                                                       //  {
+            if (Input.GetMouseButtonDown(0))                    //      當按下滑鼠左鍵
+            {                                                   //      {
+                rigidBody2d.velocity = Vector3.up * FlySpeed;   //          讓物理控制器裡面的「速度」 = (向上一個單位) x (鳥飛的速度)
+            }                                                   //      }
+        }                                                       //  }      
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if(collider.name != "point")
-        {
-            gameOver = true;
-        }
-        else
-        {
-            score++;
-            scoreTxt.text = score.ToString();
-        }
-    }
+    void OnTriggerEnter2D(Collider2D collider)      //  當與「collider」碰撞時 : 
+    {                                               //  {
+        if(collider.name == "point")                //      如果「collider」的名稱是"point"(管子中間的得分區)
+        {                                           //      {
+            score++;                                //          分數加1
+            scoreTxt.text = score.ToString();       //          文字元件內的文字 = (轉換成字串的)分數
+        }                                           //      }
+        else                                        //      如果「collider」的名稱不是"point"(也就是碰到了障礙物)
+        {                                           //      {
+            gameOver = true;                        //          遊戲結束
+        }                                           //      }
+    }                                               //  }
 }
--- Assets/Scripts/pipeController.cs
+++ Assets/Scripts/pipeController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class pipeController : MonoBehaviour
 {
     public float PipeMoveSpeed = -2.0f;
-    public float originX = 7;
+    public float originX = 7.0f;
+    public float endX = -9.0f;
 
     public float MaxHeight = 2.5f;
     public float MinHeight = -4.0f;
@@ -13,14 +14,14 @@ public class pipeController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //this.transform.position = new Vector3(0, Random.Range(-1.0f, -6.5f), 0);
+        this.transform.position = new Vector3(this.transform.position.x , Random.Range(MaxHeight, MinHeight), 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(PipeMoveSpeed * Time.deltaTime, 0, 0);
-        if (this.transform.position.x <= -9)
+        if (this.transform.position.x <= endX)
         {
             this.transform.position = new Vector3(originX, Random.Range(MaxHeight, MinHeight), 0);
         }
